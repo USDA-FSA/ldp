7d3936b4edf48f1a873e8ccb461059337d79437c
Casey Hill <bignamehere@gmail.com>
2020-09-18 14:43:43 -0500

    updates to the SMS [appends #1]

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -2,60 +2,62 @@
 const Store = (() => {
   
 
-  let keys = {
+  const keys = {
     nextPage: 'nextPage',
     prevPage: 'prevPage',
+    producerName: 'producerName',
     commodity: 'commodity',
     commodityClass: 'commodityClass',
+    appType: 'appType',
+    appNumber: 'appNumber',
+    appStatus: 'appStatus',
+    refInfo: 'refInfo'
   };
   
   const getters = {
+    getKeys: () => { return keys },
     nextPage: () => { return localStorage.getItem('nextPage') },
     prevPage: () => { return localStorage.getItem('prevPage') },
+    producerName: () => { return localStorage.getItem('producerName') },
     commodity: () => { return localStorage.getItem('commodity') },
-    commodityClass: () => { return localStorage.getItem('commodityClass') }
+    commodityClass: () => { return localStorage.getItem('commodityClass') },
+    appType: () => { return localStorage.getItem('appType') },
+    appNumber: () => { return localStorage.getItem('appNumber') },
+    appStatus: () => { return localStorage.getItem('appStatus') },
+    refInfo: () => { return localStorage.getItem('refInfo') }
   };
   
   const actions = {
-    setNextPage(data){
-      mutations.NEXT_PAGE(data)
-    },
-
-    setPrevPage(data){
-      mutations.PREV_PAGE(data)
-    },
-
-    setCommodity(data){
-      mutations.COMMODITY(data);
-    },
-
-    setCommodityClass(data){
-      mutations.COMMODITY_CLASS(data);
-    },
-
-    deleteAllData(){
-      mutations.DELETE_STORE();
-    }
+    setNextPage(data){ mutations.NEXT_PAGE(data) },
+    setPrevPage(data){ mutations.PREV_PAGE(data) },
+    setProducerName(data){ mutations.PRODUCER_NAME(data) },
+    setCommodity(data){ mutations.COMMODITY(data) },
+    setCommodityClass(data){ mutations.COMMODITY_CLASS(data) },
+    setAppType(data){ mutations.APP_TYPE(data) },
+    setAppNumber(data){ mutations.APP_NUMBER(data) },
+    setAppStatus(data){ mutations.APP_STATUS(data) },
+    setRefInfo(data){ mutations.REF_INFO(data) },
+    deleteKey(data){ mutations.DELETE_KEY(data) },
+    deleteAllData(){ mutations.DELETE_STORE() }
     
   };
   
   const mutations = {
     NEXT_PAGE(data){ localStorage.setItem('nextPage', data) },
-    
     PREV_PAGE(data){ localStorage.setItem('prevPage', data) },
-    
+    PRODUCER_NAME(data){ localStorage.setItem('producerName', data) },
     COMMODITY(data){ localStorage.setItem('commodity', data) },
-    
     COMMODITY_CLASS(data){ localStorage.setItem('commodityClass', data) },
-    
+    APP_TYPE(data){ localStorage.setItem('appType', data) },
+    APP_NUMBER(data){ localStorage.setItem('appNumber', data) },
+    APP_STATUS(data){ localStorage.setItem('appStatus', data) },
+    REF_INFO(data){ localStorage.setItem('refInfo', data) },
     DELETE_KEY(key){
       localStorage.removeItem(key);
       delete keys[key];
     },
-
     DELETE_STORE(){
       for (let key in keys) {
-        console.log('LS',localStorage.getItem(key))
         localStorage.removeItem(key);
       }
     }
@@ -63,7 +65,7 @@ const Store = (() => {
 
   return {
     getters: getters,
-    actions: actions
+    actions: actions,
   };
 
 })();
